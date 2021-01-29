0ab099d5174dcb53b62c406f0ea902b88e4b809e
nikevs <nitkevs@gmail.com>
2021-01-29 21:09:16 +0200

    Добавлен класс Player

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ *
+ * /index.php
+ *
+ * Основная страница игры для взаимодействия с игроком.
+ *
+ */
+
+require_once "/media/hosts/jotto.localhost/public_html/player.php";
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Jotto Game</title>
+  </head>
+  <body>
+    <main>
+    <h1>Jotto Game</h1>
+    <div id="alphabet">А Б В Г Д Е Ё Ж З И К Л М Н О П Р С Т У Ф Х Ц Ч Ш Щ Ъ Ы Ь Э Ю Я</div>
+    <form action="/" method="post">
+      <input type="text">
+      <button>Ответить</button>
+    </form>
+    <div id="message"></div>
+    <div id="recent-words"></div>
+    </main>
+  </body>
+  <script>
+  </script>
+</html>
+
---
+++ player.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ *
+ * /player.php
+ *
+ * Класс Player предоставляет аттрибуты
+ * для хранения данных об игроке.
+ *
+ */
+
+class Player {
+  public $answer;
+  public $alphabet;
+  public $used_words;
+}
