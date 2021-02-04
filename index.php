a88e8eef70ee661b9f9fd0c85534ee83fa8ffbdc
nikevs <nitkevs@gmail.com>
2021-02-04 16:20:42 +0200

    Игра теперь создаёт объекты игрока и скрипта, а так же алфавит корректно.
    Объекты игрока и скрипта теперь создаются новые, если начата  новая
    сессия-PHP. Или берутся из сессии, если она была начата ранее.
    Алфавит на главной теперь генерируется из объекта игрока.

--- index.php
+++ index.php
@@ -11,8 +11,27 @@
 require_once "{$_SERVER['DOCUMENT_ROOT']}/player.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/script.php";
 
-$player = new Player();
-$script = new Script();
+session_start();
+
+// Если начинается новая игра
+if (!isset($_SESSION['player'])) {
+  $player = new Player();
+  $script = new Script();
+  $script->hidden_word = $script->random_word();
+
+// Сохраняем объекты в сессии PHP
+  $_SESSION['player'] = serialize($player);
+  $_SESSION['script'] = serialize($script);
+} else {
+  $player = unserialize($_SESSION['player']);
+  $script = unserialize($_SESSION['script']);
+}
+
+$alphabet = '';
+
+foreach ($player->alphabet as $letter => $class) {
+  $alphabet .= "<span class=\"{$class} letter\">{$letter}</span>\n";
+}
 
 ?>
 <!DOCTYPE html>
@@ -24,9 +43,10 @@ $script = new Script();
   <body>
     <main>
     <h1>Jotto Game</h1>
-    <div id="alphabet">А Б В Г Д Е Ё Ж З И К Л М Н О П Р С Т У Ф Х Ц Ч Ш Щ Ъ Ы Ь Э Ю Я</div>
-    <form action="/" method="post">
-      <input type="text">
+    <div id="alphabet"><?= $alphabet ?></div>
+    <form action="check-answer.php" method="post">
+      <input type="hidden">
+      <input type="text" name="player-answer">
       <button>Ответить</button>
     </form>
     <div id="message"></div>
