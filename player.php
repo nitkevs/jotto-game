4ae533f39f0843a4e79e1a578e506a7a56b6405c
nikevs <nitkevs@gmail.com>
2021-02-04 16:25:26 +0200

    В классе Player полям заданы значения по умолчанию.
    Поля $answer и $used_words - пустые строки.
    Поле $alphabet - массив. Ключи массива - буквы, а значения -
    классы css, отражающие статус букв в игре.

--- player.php
+++ player.php
@@ -10,7 +10,41 @@
  */
 
 class Player {
-  public $answer;
-  public $alphabet;
-  public $used_words;
+  public $answer = '';
+  public $alphabet = array('А' => 'uncertain',
+                          'Б' => 'uncertain',
+                          'В' => 'uncertain',
+                          'Г' => 'uncertain',
+                          'Д' => 'uncertain',
+                          'Е' => 'uncertain',
+                          'Ё' => 'uncertain',
+                          'Ж' => 'uncertain',
+                          'З' => 'uncertain',
+                          'И' => 'uncertain',
+                          'Й' => 'uncertain',
+                          'К' => 'uncertain',
+                          'Л' => 'uncertain',
+                          'М' => 'uncertain',
+                          'Н' => 'uncertain',
+                          'О' => 'uncertain',
+                          'П' => 'uncertain',
+                          'Р' => 'uncertain',
+                          'С' => 'uncertain',
+                          'Т' => 'uncertain',
+                          'У' => 'uncertain',
+                          'Ф' => 'uncertain',
+                          'Х' => 'uncertain',
+                          'Ц' => 'uncertain',
+                          'Ч' => 'uncertain',
+                          'Ш' => 'uncertain',
+                          'Щ' => 'uncertain',
+                          'Ъ' => 'uncertain',
+                          'Ы' => 'uncertain',
+                          'Ь' => 'uncertain',
+                          'Э' => 'uncertain',
+                          'Ю' => 'uncertain',
+                          'Я' => 'uncertain',
+                          );
+
+  public $used_words =  '';
 }
