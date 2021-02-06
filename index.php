de8f942fd500deb94e1d21b420e05d13c33b2ecf
nikevs <nitkevs@gmail.com>
2021-02-06 15:43:06 +0200

    Алфавит игрока теперь интерактивен

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if (!isset($_SESSION['player'])) {
 $alphabet = '';
 
 foreach ($player->alphabet as $letter => $class) {
-  $alphabet .= "<span class=\"{$class} letter\">{$letter}</span>\n";
+  $alphabet .= "<span class=\"letter {$class}\">{$letter}</span>\n";
 }
 
 ?>
@@ -39,6 +39,43 @@ foreach ($player->alphabet as $letter => $class) {
   <head>
     <meta charset="utf-8">
     <title>Jotto Game</title>
+    <style type="text/css">
+      :root {
+        --letter-size: 24px;
+        --letter-default-color: #333;
+      }
+      * {
+        box-sizing: border-box;
+        margin: 0px;
+        padding: 0px;
+      }
+      #alphabet {
+        margin: 5px auto;
+      }
+      .letter {
+        user-select: none;
+        display: inline-block;
+        width: calc(var(--letter-size) + 2px);
+        cursor: pointer;
+        border: 1px solid transparent;
+        border-radius: 3px;
+        text-align: center;
+        line-height: var(--letter-size);
+        font-size: 20px;
+      }
+      .uncertain {
+        color: var(--letter-default-color);
+        border-color: transparent;
+      }
+      .included {
+        color: var(--letter-default-color);
+        border-color: var(--letter-default-color);
+      }
+      .excluded {
+        color: #ccc;
+        border-color: transparent;
+      }
+    </style>
   </head>
   <body>
     <main>
@@ -54,6 +91,27 @@ foreach ($player->alphabet as $letter => $class) {
     </main>
   </body>
   <script>
+    let alphabetBlock = document.getElementById('alphabet');
+    let letters = alphabetBlock.querySelectorAll('span.letter');
+    letters.forEach(function(letter){
+      letter.addEventListener('click', function(){
+        switch (true) {
+          case this.classList.contains('uncertain'):
+            this.classList.remove('uncertain');
+            this.classList.add('included');
+            break;
+          case this.classList.contains('included'):
+            this.classList.remove('included');
+            this.classList.add('excluded');
+            break;
+          case this.classList.contains('excluded'):
+            this.classList.remove('excluded');
+            this.classList.add('uncertain');
+            break;
+        }
+      });
+    });
+
   </script>
 </html>
 
